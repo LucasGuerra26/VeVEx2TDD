e1749cf0cfca808507881eb235f8fb25896c8515
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2022-10-21 10:46:16 -0300

    ajustando para passar nos testes criados anteriormente

--- CalculadoraDeSalario/src/com/company/service/CalculateRealSalaryServiceImpl.java
+++ CalculadoraDeSalario/src/com/company/service/CalculateRealSalaryServiceImpl.java
@@ -1,5 +1,11 @@
 package com.company.service;
 
+import com.company.model.Employee;
+
 public class CalculateRealSalaryServiceImpl implements CalculateRealSalaryService{
 
+    @Override
+    public void calculate(Employee employee) {
+        employee.calculateRealSalary();
+    }
 }
