07c6b4f4a767c525c8f895d697c70cd09c36c42b
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2022-10-21 10:37:12 -0300

    ajustando para passar nos testes criados anteriormente

--- CalculadoraDeSalario/src/com/company/service/UpdateOfficeServiceImpl.java
+++ CalculadoraDeSalario/src/com/company/service/UpdateOfficeServiceImpl.java
@@ -1,4 +1,10 @@
 package com.company.service;
 
+import com.company.model.Employee;
+
 public class UpdateOfficeServiceImpl implements UpdateOfficeService{
+    @Override
+    public void updateOffice(Employee employee, String newOffice) {
+        employee.setOffice(newOffice);
+    }
 }
