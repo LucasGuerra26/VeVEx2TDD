bfaf0da9beccb417a93f36faa0356fd7a605c018
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2022-10-21 09:23:06 -0300

    ajustando para passar nos primeiros testes e criando novo teste

--- CalculadoraDeSalario/src/com/company/model/Employee.java
+++ CalculadoraDeSalario/src/com/company/model/Employee.java
@@ -2,4 +2,21 @@ package com.company.model;
 
 public class Employee {
 
+    private long cpf;
+    private String name;
+    private String email;
+    private double baseSalary;
+    private String office;
+
+    public Employee(long cpf, String name, String email, double baseSalary, String office){
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.baseSalary = baseSalary;
+        this.office = office;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
--- CalculadoraDeSalario/src/com/company/model/EmployeeTest.java
+++ CalculadoraDeSalario/src/com/company/model/EmployeeTest.java
@@ -7,8 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeTest {
 
     @Test
-    void createEmployee(){
-        Employee e = new Employee("123456789", "Adeildo", "ad@g.com", 3000, "DESENVOLVEDOR");
+    void createEmployeeTest(){
+        Employee e = new Employee(123456789, "Adeildo", "ad@g.com", 3000, "DESENVOLVEDOR");
         assertEquals("Adeildo", e.getName());
+    }
+
+    @Test
+    void getTest(){
+        Employee e = new Employee(123456789, "Adeildo", "ad@g.com", 3000, "DESENVOLVEDOR");
+        assertEquals("Adeildo", e.getName());
+        assertEquals(123456789, e.getCpf());
+        assertEquals("ad@g.com", e.getEmail());
+        assertEquals(3000, e.getBaseSalary());
+        assertEquals("DESENVOLVEDOR", e.getOffice());
+
     }
 }
