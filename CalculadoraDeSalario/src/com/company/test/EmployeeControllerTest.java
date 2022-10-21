86103ab9202373aeb42a57c17404173cbcec0e9c
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2022-10-21 10:54:47 -0300

    criando testes para criar funcionario no controller

---
+++ CalculadoraDeSalario/src/com/company/test/EmployeeControllerTest.java
@@ -0,0 +1,25 @@
+package com.company.test;
+
+import com.company.controller.EmployeeController;
+import com.company.model.Employee;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+    private EmployeeController employeeController;
+
+    @BeforeEach
+    void testSetUp(){
+        employeeController = new EmployeeController();
+    }
+
+    @Test
+    void createEmployeeTest(){
+        Employee e = employeeController.createEmployee(123456789, "Adeildo", "ad@g.com", 3000, "DESENVOLVEDOR");
+        assertEquals("Adeildo", e.getName());
+        assertEquals("ad@g.com", e.getEmail());
+        assertEquals("DESENVOLVEDOR", e.getOffice());
+    }
+}
