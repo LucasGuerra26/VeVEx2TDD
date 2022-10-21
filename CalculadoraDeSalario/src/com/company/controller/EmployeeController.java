282f66287c270584968071e818ff3ac0441c4216
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2022-10-21 11:01:50 -0300

    ajustando para passar nos testes criados anteriormente

--- CalculadoraDeSalario/src/com/company/controller/EmployeeController.java
+++ CalculadoraDeSalario/src/com/company/controller/EmployeeController.java
@@ -1,5 +1,25 @@
 package com.company.controller;
 
+import com.company.model.Employee;
+import com.company.service.CreateEmployeeService;
+import com.company.service.CreateEmployeeServiceImpl;
+
+import java.util.ArrayList;
+
 public class EmployeeController {
+    private CreateEmployeeService createEmployeeService;
+    private ArrayList<Employee> employees;
+
+    public EmployeeController(){
+        createEmployeeService = new CreateEmployeeServiceImpl();
+        employees = new ArrayList<>();
+    }
+
+    public Employee createEmployee(long cpf, String name, String email, double baseSalary, String office){
+        Employee e = createEmployeeService.createEmployee(cpf, name, email, baseSalary, office);
+        employees.add(e);
+        return e;
+    }
+
 
 }
