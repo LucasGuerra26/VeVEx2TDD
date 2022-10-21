acfd514fa01a4d9c86d0ee90726ce201f0071d4f
Adeildo Pereira <adeildo.pereira@ccc.ufcg.edu.br>
2022-10-21 11:11:47 -0300

    criando testes para calcular o salario real no controller

--- CalculadoraDeSalario/src/com/company/test/EmployeeControllerTest.java
+++ CalculadoraDeSalario/src/com/company/test/EmployeeControllerTest.java
@@ -30,4 +30,43 @@ class EmployeeControllerTest {
         employeeController.updateOffice(e, "GERENTE");
         assertEquals("GERENTE", e.getOffice());
     }
+
+    @Test
+    void calculateDeveloperSalaryTest(){
+        Employee e = new Employee(123456789, "Adeildo", "ad@g.com", 3000, "DESENVOLVEDOR");
+        Employee e2 = new Employee(123456788, "Lucas", "Lu@g.com", 2500, "DESENVOLVEDOR");
+        employeeController.calculateRealSalary(e);
+        employeeController.calculateRealSalary(e2);
+        assertEquals(2400, e.getRealSalary());
+        assertEquals(2250, e2.getRealSalary());
+    }
+
+    @Test
+    void calculateDBASalaryTest(){
+        Employee e = new Employee(123456789, "Adeildo", "ad@g.com", 3000, "DBA");
+        Employee e2 = new Employee(123456788, "Lucas", "Lu@g.com", 1500, "DBA");
+        employeeController.calculateRealSalary(e);
+        employeeController.calculateRealSalary(e2);
+        assertEquals(2250, e.getRealSalary());
+        assertEquals(1275, e2.getRealSalary());
+    }
+
+    @Test
+    void calculateSalaryTesterTest(){
+        Employee e = new Employee(123456789, "Adeildo", "ad@g.com", 3000, "TESTADOR");
+        Employee e2 = new Employee(123456788, "Lucas", "Lu@g.com", 1500, "TESTADOR");
+        employeeController.calculateRealSalary(e);
+        employeeController.calculateRealSalary(e2);
+        assertEquals(2250, e.getRealSalary());
+        assertEquals(1275, e2.getRealSalary());
+    }
+    @Test
+    void calculateManagerSalaryTest(){
+        Employee e = new Employee(123456789, "Adeildo", "ad@g.com", 6000, "GERENTE");
+        Employee e2 = new Employee(123456788, "Lucas", "Lu@g.com", 4500, "GERENTE");
+        employeeController.calculateRealSalary(e);
+        employeeController.calculateRealSalary(e2);
+        assertEquals(4200, e.getRealSalary());
+        assertEquals(3600, e2.getRealSalary());
+    }
 }
